093682add3aa78ecb9418cda0e98db61a242eb4e
Nahuel Zalazar <zvnahuel63@gmail.com>
2023-07-01 20:48:19 -0400

    fixed deploay

--- db/connnection.ts
+++ db/connnection.ts
@@ -1,31 +1,25 @@
 import { Options, Sequelize } from "sequelize";
 
-const user = encodeURIComponent(process.env.DB_USER || "");
-const password = encodeURIComponent(process.env.DB_PASSWORD || "");
-const host = encodeURIComponent(process.env.DB_HOST || "");
-const port = encodeURIComponent(process.env.DB_PORT || "");
-const database = encodeURIComponent(process.env.DB_DATABASE || "");
-const typeDatabase = "postgres";
+const user = 'pubttwrcsdxvgl';
+const password = '1b9a3379ca07066c24e47e27be4916ca2cdfbd9083f96c4c8a47dc94b082c4ac';
+const host = 'ec2-34-197-91-131.compute-1.amazonaws.com';
+const port = 5432;
+const database = 'd2pprl600mfs20';
+const typeDatabase = 'postgres';
 
 const url = `${typeDatabase}://${user}:${password}@${host}:${port}/${database}`;
 
+console.log(url);
+
 const options: Options = {
-  dialect: "postgres", // 'mysql' | 'sqlite' | 'postgres' | 'mariadb' | 'mssql'
-  logging: false, // false
-  dialectOptions: {
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  },
+    dialect: 'postgres', // 'mysql' | 'sqlite' | 'postgres' | 'mariadb' | 'mssql'
+    logging: false, // false
+    dialectOptions: {
+        ssl: {
+            rejectUnauthorized: false
+        }
+    }
 };
 
 const sequelizeConnection = new Sequelize(url, options);
 export default sequelizeConnection;
-
-//     PARA EL LOCALHOST
-// const db = new Sequelize('houseSales', 'postgres', 'password', {
-//     host: 'localhost',
-//     dialect: "postgres",
-// })
-
-// export default db;
