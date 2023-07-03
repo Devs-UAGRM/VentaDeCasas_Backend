6d3fb16d1861537ca8245efae6cd0f4d7e9f8c19
runinbk <runinbk@gmail.com>
2023-07-03 01:13:57 -0400

    Actualizacion de las ENV_DB

--- dist/db/connnection.js
+++ dist/db/connnection.js
@@ -1,11 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
-const user = 'pubttwrcsdxvgl';
-const password = '1b9a3379ca07066c24e47e27be4916ca2cdfbd9083f96c4c8a47dc94b082c4ac';
-const host = 'ec2-34-197-91-131.compute-1.amazonaws.com';
+const user = 'jwyjpayxggfxea';
+const password = 'e2915dc6f998be73d3a1c40b7a4857a2a556162504017ad26c2881b819b4e0d1';
+const host = 'ec2-3-218-172-130.compute-1.amazonaws.com';
 const port = 5432;
-const database = 'd2pprl600mfs20';
+const database = 'd7adg1n2jf2rqt';
 const typeDatabase = 'postgres';
 const url = `${typeDatabase}://${user}:${password}@${host}:${port}/${database}`;
 console.log(url);
